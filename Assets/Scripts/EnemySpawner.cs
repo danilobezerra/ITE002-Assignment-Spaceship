518a2c4455b1340bfefc853143316bb19160dd78
20galmeida <20galmeida@gmail.com>
2024-12-01 15:50:22 -0300

    fix spawn bug (spawning too close to player)

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -9,18 +9,30 @@ public class EnemySpawner : MonoBehaviour
     public float yMin = -4f;
     public float yMax = 4f;
 
+    public float minDistanceFromPlayer = 5f;
+
+    private Transform playerTransform;
+
     void Start()
     {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        playerTransform = player.transform;
+
         SpawnEnemy();
     }
 
     public void SpawnEnemy()
     {
-        Vector3 spawnPosition = new(
-            Random.Range(xMin, xMax),
-            Random.Range(yMin, yMax),
-            0f
-        );
+        Vector3 spawnPosition;
+        do
+        {
+            spawnPosition = new Vector3(
+                Random.Range(xMin, xMax),
+                Random.Range(yMin, yMax),
+                0f
+            );
+
+        } while (Vector3.Distance(spawnPosition, playerTransform.position) < minDistanceFromPlayer);
 
         Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
     }
